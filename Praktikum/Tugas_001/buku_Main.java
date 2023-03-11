e3638280728ae0fa459839c5db09e8872f49f9a5
Syahreza <syahrezafistiferdian32@gmail.com>
2023-03-11 18:08:31 +0700

    [CHANGE] Main program is no longer use Array Copy method

--- Praktikum/Tugas_001/buku_Main.java
+++ Praktikum/Tugas_001/buku_Main.java
@@ -1,6 +1,5 @@
 package Praktikum.Tugas_001;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class buku_Main {
@@ -8,7 +7,6 @@ public class buku_Main {
     public static void main(String[] args) {
         int pilihan;
         //judul, kategori, penulis, jml halaman, tahun terbit
-        String[][] dummyData = new String[10][5];
         String[][] Agama = {{"Pendidikan Agama Islam", "Mr. X", "12", "2021"},
                                 {"Pendidikan Agama Kristen", "Mr. Y", "13", "2017"},
                                 {"Pendidikan Agama Hindu", "Mr. Z", "14", "2015"},
@@ -18,8 +16,7 @@ public class buku_Main {
                                 {"Java Programming", "Mr. X", "224", "2018"},
                                 {"Belajar C++", "Mr.X", "225", "2018"}, 
                                 {"Mahir AutoCad", "Mr. X", "340", "2016"},
-                                {"Dasar Pengoperasian MS. Office", "Mr. X", "225", "2018"}};
-        buku bukuN = new buku();       
+                                {"Dasar Pengoperasian MS. Office", "Mr. X", "225", "2018"}};     
 
         do {
             System.out.println("=======[Perpustakaan]========");
@@ -28,35 +25,30 @@ public class buku_Main {
             System.out.print("Pilihan anda: ");
             pilihan = scanner.nextInt();
             
-            if(pilihan != 8){
-                switch(pilihan){
-                    case 1: {
-                        dummyData = Arrays.copyOf(Teknologi, Teknologi.length); 
-                        bukuN.setKategori("Teknologi");
-                        bukuN.tampilkanKategori(); 
-                        break;
-                    }
-                    case 4: {
-                        dummyData = Arrays.copyOf(Agama, Agama.length);
-                        bukuN.setKategori("Agama");
-                        bukuN.tampilkanKategori(); 
-                        break;
-                    }
-                }
-    
-                for(int i = 0; i < dummyData.length; i++){
-                    for(int j = 0; j < dummyData[i].length; j++){
-                        bukuN.setNamaBuku(dummyData[i][0]);
-                        bukuN.setNamaPenulis(dummyData[i][1]);
-                        bukuN.setJumlahHalaman(dummyData[i][2]);
-                        bukuN.setTahunTerbit(dummyData[i][3]);
-                        bukuN.setNomorBuku(String.valueOf(i+1));
-                    }
-                    bukuN.tampilkan();
-                }
+            switch (pilihan) {
+                case 1: loopAndSetData(Teknologi, "Teknologi"); break;
+                case 4: loopAndSetData(Agama, "Agama"); break;
+                default: if(pilihan != 8) System.out.println("ERROR: Masukkan pilihan yang benar!");
             }
+            
         } while (pilihan != 8);
         System.out.println("USERINFO: Program Berhenti");
+    }
+
+    public static void loopAndSetData(String[][]data, String kategoriBuku){
+        buku bukuN = new buku();
+        bukuN.setKategori(kategoriBuku);
+        bukuN.tampilkanKategori();
+        for(int i = 0; i < data.length; i++){
+            for(int j = 0; j < data.length; j++){
+                bukuN.setNamaBuku(data[i][0]);
+                bukuN.setNamaPenulis(data[i][1]);
+                bukuN.setJumlahHalaman(data[i][2]);
+                bukuN.setTahunTerbit(data[i][3]);
+                bukuN.setNomorBuku(String.valueOf(i+1));
+            }
+            bukuN.tampilkan();
+        }
     }
 }
 

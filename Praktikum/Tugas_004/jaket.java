114aee5c51b4dba38f699829e5cd8ca73ec81aa3
Syahreza <syahrezafistiferdian32@gmail.com>
2023-04-22 00:14:03 +0700

    [CHANGE] Variable naming correction

--- Praktikum/Tugas_004/jaket.java
+++ Praktikum/Tugas_004/jaket.java
@@ -2,11 +2,11 @@ package Tugas_004;
 
 public class jaket {
     //                          harga_awal, harga_stlh_diskon
-    private final int jaketA[] = {100_000, 95_000};
-    private final int jaketB[] = {125_000, 120_000};
-    private final int jaketC[] = {175_000, 160_000};
+    private final int HARGA_JAKET_A[] = {100_000, 95_000};
+    private final int HARGA_JAKET_B[] = {125_000, 120_000};
+    private final int HARGA_JAKET_C[] = {175_000, 160_000};
 
-    public int[] getJaket(char jaket){
-        return (jaket == 'A') ? jaketA : ((jaket == 'B') ? jaketB : jaketC);
+    public int[] getHargaJaket(char jaket){
+        return (jaket == 'A') ? HARGA_JAKET_A : ((jaket == 'B') ? HARGA_JAKET_B : HARGA_JAKET_C);
     }
 }

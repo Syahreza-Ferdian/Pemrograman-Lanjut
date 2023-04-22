0d678e0e607a8c012109b897cd81310db87c6be3
Syahreza <syahrezafistiferdian32@gmail.com>
2023-04-22 19:16:15 +0700

    [CHANGE] Maximum jaket type as instance var.

--- Praktikum/Tugas_004/jaket.java
+++ Praktikum/Tugas_004/jaket.java
@@ -1,6 +1,7 @@
 package Tugas_004;
 
 public class jaket {
+    final char MAX_JACKET_TYPE = 'C';
     //                          harga_awal, harga_stlh_diskon
     private final int HARGA_JAKET_A[] = {100_000, 95_000};
     private final int HARGA_JAKET_B[] = {125_000, 120_000};

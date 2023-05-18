bc47713137450bfe47cda39a61970b4c69452034
Syahreza <syahrezafistiferdian32@gmail.com>
2023-05-18 12:36:38 +0700

    [ADD] Penambahan test case sesuai soal

--- Praktikum/Tugas_005/mainProg.java
+++ Praktikum/Tugas_005/mainProg.java
@@ -13,5 +13,34 @@ public class mainProg {
 
         // manager d = new manager("HRD", 1000, 2017, 1, 2, 3, "D", "111", true, true);
         // System.out.println(d);
+
+        //TEST CASE SESUAI SOAL
+        manusia hooman1 = new manusia("Slamet Raharjo, S.Pd., M.Pd., M.Si", "45670192", true, true);
+        manusia hooman2 = new manusia("Ibu Sriani", "689821309", false, true);
+        manusia hooman3 = new manusia("Mas Adi", "890831094", true, false);
+
+        mahasiswaFILKOM saya1 = new mahasiswaFILKOM("225150701111036", 2.8, "Syahreza Fisti Ferdian", "56712093", true, false);
+        mahasiswaFILKOM saya2 = new mahasiswaFILKOM("225150701111036", 3.3, "Syahreza Fisti Ferdian", "56712093", true, false);
+        mahasiswaFILKOM saya3 = new mahasiswaFILKOM("225150701111036", 3.9, "Syahreza Fisti Ferdian", "56712093", true, false);
+
+        pekerja pekerja1 = new pekerja(5000, 2021, 01, 25, 2, "Slamet", "92180411", true, true);
+        pekerja pekerja2 = new pekerja(5000, 2014, 02, 24, 0, "Yuli", "891209312", false, false);
+        pekerja pekerja3 = new pekerja(5000, 2003, 04, 15, 10, "Bpk. Santoso", "891831021", true, true);
+
+        manager manager1 = new manager("Quality Assurance", 7500, 2008, 03, 28, 0, "Achmad, S.T., M.T., Ph.D", "1453241", true, false);
+
+        // System.out.println(hooman1);
+        // System.out.println(hooman2);
+        // System.out.println(hooman3);
+
+        // System.out.println(saya1);
+        // System.out.println(saya2);
+        // System.out.println(saya3);
+
+        // System.out.println(pekerja1);
+        // System.out.println(pekerja2);
+        // System.out.println(pekerja3);
+
+        // System.out.println(manager1);
     }
 }
